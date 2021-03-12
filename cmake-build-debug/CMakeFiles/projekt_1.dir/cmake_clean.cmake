0ebf0d1611bbaf577c0f3b06dbfa4706ced87a76
HuNNi31 <csegzi_huni@yahoo.com>
2021-03-12 10:10:59 +0200

    ^1

---
+++ cmake-build-debug/CMakeFiles/projekt_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt_1.dir/main.c.obj"
+  "projekt_1.exe"
+  "projekt_1.exe.manifest"
+  "projekt_1.lib"
+  "projekt_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projekt_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
